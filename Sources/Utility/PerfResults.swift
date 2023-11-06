78ba339b8a23f6ba65054fc8aabc4aee9388a7f4
Erika Foxelot <erikafoxelot@gmail.com>
2023-11-06 00:04:38 -0800

    formatting

--- Sources/Utility/PerfResults.swift
+++ Sources/Utility/PerfResults.swift
@@ -1,18 +1,18 @@
 import Foundation
 
 struct PerfResults: CustomStringConvertible {
-  var duration: Double = 0
-  var tokensSent: Int = 0
-  var tokensReceived: Int = 0
-  var completedSuccessfully: Bool = false
+    var duration: Double = 0
+    var tokensSent: Int = 0
+    var tokensReceived: Int = 0
+    var completedSuccessfully: Bool = false
 
-  var description: String {
-    return
-      """
-      Completed Successfully: \(completedSuccessfully)
-      Duration: \(Int(duration * 1000))ms
-      Tokens Sent: \(tokensSent)
-      Tokens Received: \(tokensReceived)
-      """
-  }
+    var description: String {
+        return
+            """
+            Completed Successfully: \(completedSuccessfully)
+            Duration: \(Int(duration * 1000))ms
+            Tokens Sent: \(tokensSent)
+            Tokens Received: \(tokensReceived)
+            """
+    }
 }
