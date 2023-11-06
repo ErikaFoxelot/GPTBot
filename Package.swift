21d65af590847e6f2b6c2c96578b8ef77c370061
Erika Foxelot <erikafoxelot@gmail.com>
2023-11-06 00:04:38 -0800

    formatting

--- Package.swift
+++ Package.swift
@@ -2,24 +2,24 @@
 import PackageDescription
 
 let package = Package(
-  name: "GPTBot",
-  platforms: [.macOS(.v10_15)],
-  products: [
-    .executable(name: "gptbot", targets: ["gptbot"])
-  ],
-  dependencies: [
-    .package(url: "https://github.com/ErikaFoxelot/GPTEncoder.git", branch: "main"),
-    .package(url: "https://github.com/MacPaw/OpenAI.git", from: "0.2.4"),
-    .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.3"),
-  ],
-  targets: [
-    .executableTarget(
-      name: "gptbot",
-      dependencies: [
-        "GPTEncoder",
-        "OpenAI",
-        .product(name: "ArgumentParser", package: "swift-argument-parser"),
-      ]
-    )
-  ]
+    name: "GPTBot",
+    platforms: [.macOS(.v10_15)],
+    products: [
+        .executable(name: "gptbot", targets: ["gptbot"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/ErikaFoxelot/GPTEncoder.git", branch: "main"),
+        .package(url: "https://github.com/MacPaw/OpenAI.git", from: "0.2.4"),
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.3"),
+    ],
+    targets: [
+        .executableTarget(
+            name: "gptbot",
+            dependencies: [
+                "GPTEncoder",
+                "OpenAI",
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+            ]
+        )
+    ]
 )
