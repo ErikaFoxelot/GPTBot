ef2a2aa04184fa30df7d39dfdffdc17710420d66
Erika Foxelot <erikafoxelot@gmail.com>
2023-11-06 00:02:57 -0800

    forked GPTEncoder to fix bundle dependency issue (pending)

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     .executable(name: "gptbot", targets: ["gptbot"])
   ],
   dependencies: [
-    .package(url: "https://github.com/alfianlosari/GPTEncoder.git", from: "1.0.3"),
+    .package(url: "https://github.com/ErikaFoxelot/GPTEncoder.git", branch: "main"),
     .package(url: "https://github.com/MacPaw/OpenAI.git", from: "0.2.4"),
     .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.3"),
   ],
@@ -19,8 +19,7 @@ let package = Package(
         "GPTEncoder",
         "OpenAI",
         .product(name: "ArgumentParser", package: "swift-argument-parser"),
-      ],
-      path: "Sources"
+      ]
     )
   ]
 )
